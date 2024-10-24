10493c3ba5c5d62b37700c1938617d238c823681
StanV <48187702+TheStanster@users.noreply.github.com>
2024-10-24 18:44:41 +0200

    Set up Spring Boot project with Gradle
    Added essential configurations for a Spring Boot application using Gradle including the build script, wrapper, and application properties. Also included .gitignore and .gitattributes files to manage project-specific settings and ignored files. The initial Java class and a basic test class were created to bootstrap the application.

---
+++ src/main/java/org/sv/exceltopojo/ExcelToPojoApplication.java
@@ -0,0 +1,13 @@
+package org.sv.exceltopojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelToPojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelToPojoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sv/exceltopojo/ExcelToPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sv.exceltopojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelToPojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
